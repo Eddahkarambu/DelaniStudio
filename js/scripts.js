e1bdbc2d9f332b79829e0f2bd297d95da31bf3f4
Eddahkarambu <gikunaeddah@gmail.com>
2021-06-18 10:28:22 +0300

    added css styles,html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".design").click(function(){
+    $(".P").toggle();
+    $(".I").toggle();
+
+  });
+
+  $(".icon").click(function(){
+    $(".M").toggle();
+    $(".D").toggle();
+
+  })
+  $(".product").click(function(){
+    $(".G").toggle();
+    $(".Pr").toggle();
+
+  })
+
+
+});
